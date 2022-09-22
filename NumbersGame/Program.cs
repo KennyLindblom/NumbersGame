8348ddaf9aca8e05cb063f5a9a4021f3df6b751a
Kenny Lindblom <kennylindblom93@gmail.com>
2022-09-22 14:47:47 +0200

    Skapade två nya funktioner med en högre svårighetsgrad. Har ändrat i variblarna så att Random numret har ett högre spann. Även ändrat antalet gissningar som är tillåtet i svårighetsgrad Medium och Hard för att göra spelet möjligt att vinna.

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -108,6 +108,177 @@ namespace NumbersGame
 
         }
 
+        public static void CheckGuessMedium()
+        {
+            Random r = new Random();
+            int rättnummer = r.Next(1, 50);
+            bool win = false;
+            int maxGuess = 10;
+            int count = 0;
+
+
+            do
+
+            {
+                if (count == maxGuess)
+                {
+                    Console.WriteLine("Slut på gissningar.....");
+
+                    break;
+                }
+
+
+                Console.WriteLine("Gissa ett nummer mellan 1 och 50 du har " + maxGuess + " försök på dig");
+
+
+
+                int guess = int.Parse(Console.ReadLine());
+
+
+                if (guess > rättnummer)
+
+                {
+
+                    Console.WriteLine("För högt! Gissa lägre....");
+                    count++;
+
+
+                }
+
+                else if (guess < rättnummer)
+                {
+
+                    Console.WriteLine("För lågt!  Gisssa högre");
+                    count++;
+
+                }
+
+                else if (guess == rättnummer)
+                {
+                    Console.WriteLine("DU VANN!!!!");
+                    count++;
+
+
+                    win = true;
+                    Console.WriteLine("Vill du spela igen? ja eller nej"); //Försök komma på ett sätt att starta om spelet 
+                    string UserDeciPlay = Console.ReadLine().ToUpper();    //Jag vill att användaren skriver ja så kommer vi tillbka till 
+                                                                           // svårighetsgrad väljaren och om nej skrivs " Tack för att du spelade ut"
+                    if (UserDeciPlay.ToLower() == "ja")
+
+
+
+                    {
+
+                        Console.Clear();
+
+                        ChoseDif();
+
+
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tack för att du spelade");
+                    }
+
+
+                }
+
+
+
+
+            } while (win == false);
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+        public static void CheckGuessHard()
+        {
+            Random r = new Random();
+            int rättnummer = r.Next(1, 100);
+            bool win = false;
+            int maxGuess = 15;
+            int count = 0;
+
+
+            do
+
+            {
+                if (count == maxGuess)
+                {
+                    Console.WriteLine("Slut på gissningar.....");
+
+                    break;
+                }
+
+
+                Console.WriteLine("Gissa ett nummer mellan 1 och 100 du har " + maxGuess + " Försök på dig");
+
+
+
+                int guess = int.Parse(Console.ReadLine());
+
+
+                if (guess > rättnummer)
+
+                {
+
+                    Console.WriteLine("För högt! Gissa lägre....");
+                    count++;
+
+
+                }
+
+                else if (guess < rättnummer)
+                {
+
+                    Console.WriteLine("För lågt!  Gisssa högre");
+                    count++;
+
+                }
+
+                else if (guess == rättnummer)
+                {
+                    Console.WriteLine("DU VANN!!!! ");
+                    count++;
+
+                    win = true;
+                    Console.WriteLine("Vill du spela igen? ja eller nej"); //Försök komma på ett sätt att starta om spelet 
+                    string UserDeciPlay = Console.ReadLine().ToUpper();    //Jag vill att användaren skriver ja så kommer vi tillbka till 
+                                                                           // svårighetsgrad väljaren och om nej skrivs " Tack för att du spelade ut"
+                    if (UserDeciPlay.ToLower() == "ja")
+
+
+
+                    {
+
+                        Console.Clear();
+
+                        ChoseDif();
+
+
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tack för att du spelade");
+                    }
+
+                }
+
+            } while (win == false);
+
+            Console.ReadKey();
+        }
+
 
 
     }
