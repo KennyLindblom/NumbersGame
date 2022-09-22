dc89860a47a2abb9004aa5a835dab785076e55e4
Kenny Lindblom <kennylindblom93@gmail.com>
2022-09-22 14:42:49 +0200

    Skapade logik för själva spelet. Vad som ska skrivas ut när spelaren gör en gissning och vad som händer om man går över antalet gissningar som är tillåtet

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -13,5 +13,102 @@ namespace NumbersGame
 
             
         }
+
+        public static void CheckGuessEasy()
+        {
+            Random r = new Random();
+            int rättnummer = r.Next(1, 20);
+            bool win = false;
+            int maxGuess = 5;
+            int count = 0;
+
+
+            do
+
+            {
+                if (count == maxGuess)
+                {
+                    Console.WriteLine("Slut på gissningar.....");
+
+                    break;
+                }
+
+
+                Console.WriteLine("Gissa ett nummer mellan 1 och 20 ");
+
+
+
+                int guess = int.Parse(Console.ReadLine());
+
+
+                if (guess > rättnummer)
+
+                {
+
+                    Console.WriteLine("För högt! Gissa lägre....");
+                    count++;
+
+
+                }
+
+                else if (guess < rättnummer)
+                {
+
+                    Console.WriteLine("För lågt!  Gisssa högre");
+                    count++;
+
+                }
+
+                else if (guess == rättnummer)
+                {
+                    Console.WriteLine("DU VANN!!!! ");
+                    count++;
+
+
+                    win = true;
+
+                    Console.WriteLine("Vill du spela igen? ja eller nej"); //Försök komma på ett sätt att starta om spelet 
+                    string UserDeciPlay = Console.ReadLine().ToUpper();    //Jag vill att användaren skriver ja så kommer vi tillbka till 
+                                                                           // svårighetsgrad väljaren och om nej skrivs " Tack för att du spelade ut"
+                    if (UserDeciPlay.ToLower() == "ja")
+
+
+
+                    {
+
+                        Console.Clear();
+
+                        ChoseDif();
+
+
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tack för att du spelade");
+                    }
+
+
+
+
+                }
+
+
+
+
+            } while (win == false);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+
+
     }
 }
